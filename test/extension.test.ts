30bffd50c3a354478ab05405bcf47b43386fba46
Adrien Brizard <brizard.adrien@gmail.com>
2016-07-06 14:46:06 +0200

    Clean up of useless comments

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,7 +2,6 @@
 import * as assert from 'assert';
 import {Dash} from '../src/dash';
 import {platform} from 'os';
-//import * as mockery from 'mockery';
 
 var platforms = {
     'win32': 'cmd /c start ""',
@@ -32,28 +31,8 @@ suite("Dash Tests", () => {
     });
 
     test('Get platform command line ', () => {
-        // iterate over keys
-        /*for (var k in platforms) {
-            let stubOs = {
-                'platform': function () { 
-                    console.log(`returning ${k}`);
-                    return k; 
-                }
-            };
-            // mock call to os.platform() to return a specific platform for tests
-            mockery.registerMock('os', stubOs);
-            mockery.enable({ useCleanCache: true });
-            let dash = new Dash();
-            let command = dash.getPlatformCommand();
-            console.log(`testing with platform ${k}: ${platforms[k]}`);
-            console.log(`actual is ${command}`);
-            assert.equal(command, platforms[k]);
-            mockery.deregisterMock('os');
-            mockery.disable();
-        }*/
         let dash = new Dash();
-        let command = dash.getPlatformCommand();
-        assert.equal(command, platforms[platform()]);
+        assert.equal(dash.getPlatformCommand(), platforms[platform()]);
     });
 
     test('Get keys with exist docset', () => {
